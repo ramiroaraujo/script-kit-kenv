e5ae205e7b5d1e154813a34571510435faea23ea
Ramiro Araujo <rama.araujo@gmail.com>
2023-04-21 15:21:17 -0300

    add type clipboard script

---
+++ scripts/type-clipboard.ts
@@ -0,0 +1,20 @@
+// Name: Type Clipboard
+// Description: Get the content of the clipboard and "keystroke" it without pasting
+// Shortcut: ctrl+cmd+alt+shift+v
+
+import "@johnlindquist/kit"
+
+const clipboardText = await clipboard.readText()
+
+if (clipboardText.length > 1000) {
+    await notify("Clipboard content is too long")
+    exit()
+}
+
+await applescript(String.raw`
+    set chars to count (get the clipboard)
+    tell application "System Events"
+        delay 0.1
+        keystroke (get the clipboard)
+    end tell
+`)
