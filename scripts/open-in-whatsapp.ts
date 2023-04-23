0d8131f178094d17230e85595de68ee44c861be9
Ramiro Araujo <rama.araujo@gmail.com>
2023-04-23 18:36:53 -0300

    add simple open in whatsapp script

---
+++ scripts/open-in-whatsapp.ts
@@ -0,0 +1,23 @@
+// Name: Open in WhatsApp
+
+import "@johnlindquist/kit"
+
+//get the text from the clipboard
+let text = await clipboard.readText();
+
+//normalize the text
+text = text.replace(/[-() ]/g, "");
+
+//validate if valid phone number
+if (!text.match(/^(\+\d{12,13})|(\d{10,11})$/)) {
+  notify("Invalid phone number");
+  exit()
+}
+
+//assume Argentina if no country code since that's where I'm from
+if (!text.startsWith("+")) {
+  text = "+54" + text;
+}
+
+//open in WhatsApp
+open(`https://wa.me/${text}`);
