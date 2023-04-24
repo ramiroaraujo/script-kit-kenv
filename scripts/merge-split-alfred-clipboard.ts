e87aadd9c0f6ceab741389c89ec89e4caeed76d0
Ramiro Araujo <rama.araujo@gmail.com>
2023-04-24 12:42:40 -0300

    add preview to merge/split clipboard

--- scripts/merge-split-alfred-clipboard.ts
+++ scripts/merge-split-alfred-clipboard.ts
@@ -5,6 +5,11 @@ import "@johnlindquist/kit"
 
 const Database = await npm("better-sqlite3");
 const path = "/Users/ramiroaraujo/Library/Application Support/Alfred/Databases/clipboard.alfdb";
+if (!await pathExists(path)) {
+    notify("Alfred clipboard database not found" )
+    exit()
+}
+
 const db = new Database(path);
 
 const queryClipboard = async (sql, params) => {
@@ -12,17 +17,22 @@ const queryClipboard = async (sql, params) => {
     return sql.trim().toUpperCase().startsWith("SELECT") ? stmt.all(params) : stmt.run(params);
 };
 
-const mergeClipboards = async (count, separator) => {
+const getMergedClipboards = async (count, separator) => {
     const sql = `SELECT item FROM clipboard WHERE dataType = 0 order by ROWID desc LIMIT ?`;
     const clipboards = await queryClipboard(sql, [count]);
-    const mergedText = clipboards.map(row => row.item.trim()).join(separator);
+    return clipboards.map(row => row.item.trim()).join(separator);
+};
+
+const writeMergedClipboards = async (mergedText) => {
     await clipboard.writeText(mergedText);
 };
 
-const splitClipboard = async (separator, trim) => {
+const getSplitClipboard = async (separator, trim) => {
     const currentClipboard = await clipboard.readText();
-    const splitText = currentClipboard.split(separator).map(item => trim ? item.trim() : item);
+    return currentClipboard.split(separator).map(item => trim ? item.trim() : item);
+};
 
+const writeSplitClipboard = async (splitText) => {
     const lastTsSql = `SELECT ts FROM clipboard WHERE dataType = 0 ORDER BY ts DESC LIMIT 1`;
     const lastTsResult = await queryClipboard(lastTsSql, []);
     let lastTs = lastTsResult.length > 0 ? Number(lastTsResult[0].ts) : 0;
@@ -37,17 +47,36 @@ const splitClipboard = async (separator, trim) => {
     await clipboard.writeText(splitText[splitText.length - 1]);
 };
 
+
 const action = await arg("Choose action", ["Merge", "Split"]);
 
 if (action === "Merge") {
-    const count = await arg("Enter the number of clipboard items to merge" );
-    const separator = await arg("Enter the separator for merging");
-    await mergeClipboards(count, separator);
+    const count = await arg({
+        placeholder: "Enter the number of clipboard items to merge",
+    }, async (input) => {
+        if (isNaN(Number(input)) || input.length === 0)return ''
+        return md(`<pre>${await getMergedClipboards(input, '\n')}</pre>`)
+    })
+    const separator = await arg({
+        placeholder: "Enter the separator for merging",
+    }, async (input) => {
+        if (input === '\\n') input = '\n'
+        return md(`<pre>${await getMergedClipboards(count, input)}</pre>`)
+    })
+    const mergedText = await getMergedClipboards(count, separator);
+    await writeMergedClipboards(mergedText);
     await notify("Merged clipboard items and copied to clipboard");
 } else {
-    const separator = await arg("Enter the separator for splitting");
+    // const separator = await arg("Enter the separator for splitting");
+    const separator = await arg({
+        placeholder: "Enter the separator for splitting",
+    }, async (input) => {
+        if (input === '\\n') input = '\n'
+        return md(`<pre>${await getSplitClipboard(input, true)}</pre>`)
+    })
     const trim = await arg("Trim clipboard content?", ["Yes", "No"]);
-    await splitClipboard(separator, trim === "Yes");
+    const splitText = await getSplitClipboard(separator, trim === "Yes");
+    await writeSplitClipboard(splitText);
     await notify("Split clipboard content and stored in Alfred clipboard");
 }
 
