cdb1cc1a61884eca8368ecd32470163fdae682fd
Ramiro Araujo <rama.araujo@gmail.com>
2023-04-24 12:22:14 -0300

    blindly add win and linux support to ocr script

--- scripts/ocr.ts
+++ scripts/ocr.ts
@@ -3,9 +3,32 @@
 
 import "@johnlindquist/kit";
 
+//both win and linux implementations were created by chatgpt (gpt4), without _any_ tests!! 😅
 const captureScreenshot = async () => {
     const tmpFile = `/tmp/screenshot-${Date.now()}.png`;
-    await exec(`screencapture -i ${tmpFile}`);
+
+    if (isMac) {
+        await exec(`screencapture -i ${tmpFile}`);
+    } else if (isWin) {
+        const psScript = `
+      Add-Type -AssemblyName System.Windows.Forms
+      [System.Windows.Forms.SendKeys]::SendWait('%{PRTSC}')
+      Start-Sleep -m 500
+      $clipboardData = Get-Clipboard -Format Image
+      $clipboardData.Save('${tmpFile}', [System.Drawing.Imaging.ImageFormat]::Png)
+    `;
+        await exec(`powershell -Command "${psScript.replace(/\n/g, '')}"`);
+    } else if (isLinux) {
+        // Check if gnome-screenshot is available
+        try {
+            await exec('gnome-screenshot --version');
+            await exec(`gnome-screenshot -f ${tmpFile}`);
+        } catch (error) {
+            // If gnome-screenshot is not available, try using ImageMagick's 'import' command
+            await exec(`import ${tmpFile}`);
+        }
+    }
+
     return tmpFile;
 };
 
