0c4adc46ebde2658b0513e0aa59e3c04d4264915
Ramiro Araujo <rama.araujo@gmail.com>
2023-04-24 12:44:55 -0300

    fix array to string

--- scripts/merge-split-alfred-clipboard.ts
+++ scripts/merge-split-alfred-clipboard.ts
@@ -72,7 +72,8 @@ if (action === "Merge") {
         placeholder: "Enter the separator for splitting",
     }, async (input) => {
         if (input === '\\n') input = '\n'
-        return md(`<pre>${await getSplitClipboard(input, true)}</pre>`)
+        let strings = await getSplitClipboard(input, true);
+        return md(`<pre>${strings.join('\n')}</pre>`)
     })
     const trim = await arg("Trim clipboard content?", ["Yes", "No"]);
     const splitText = await getSplitClipboard(separator, trim === "Yes");
