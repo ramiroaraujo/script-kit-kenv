9a3daae6dfdbbded9497bf9b4a08f9490b3a78b8
Ramiro Araujo <rama.araujo@gmail.com>
2023-04-24 00:07:44 -0300

    update ff-gcp to fetch data from cloud run instances, storage buckets and secrets

--- scripts/ff-gcp.ts
+++ scripts/ff-gcp.ts
@@ -3,13 +3,48 @@
 import "@johnlindquist/kit"
 
 const urls = [
-    { name: 'Cloud Run', value: 'https://console.cloud.google.com/run?project='},
-    { name: 'Cloud Scheduler', value: 'https://console.cloud.google.com/cloudscheduler?project='},
-    { name: 'Firestore', value: 'https://console.cloud.google.com/firestore?project='},
-    { name: 'Logs', value: 'https://console.cloud.google.com/logs/query?project='},
-    { name: 'Errors', value: 'https://console.cloud.google.com/errors?project='},
-    { name: 'Storage', value: 'https://console.cloud.google.com/storage/browser?project='},
-    { name: 'Secret Manager', value: 'https://console.cloud.google.com/security/secret-manager?project='},
+    {
+        name: 'Cloud Run', value: {
+            url: 'https://console.cloud.google.com/run?project=',
+            type: 'run'
+        }
+    },
+    {
+        name: 'Cloud Scheduler', value: {
+            url: 'https://console.cloud.google.com/cloudscheduler?project=',
+            type: 'scheduler'
+        }
+    },
+    {
+        name: 'Firestore', value: {
+            url: 'https://console.cloud.google.com/firestore?project=',
+            type: null
+        }
+    },
+    {
+        name: 'Logs', value: {
+            url: 'https://console.cloud.google.com/logs/query?project=',
+            type: null
+        }
+    },
+    {
+        name: 'Errors', value: {
+            url: 'https://console.cloud.google.com/errors?project=',
+            type: null
+        }
+    },
+    {
+        name: 'Storage', value: {
+            url: 'https://console.cloud.google.com/storage/browser?project=',
+            type: 'storage'
+        }
+    },
+    {
+        name: 'Secret Manager', value: {
+            url: 'https://console.cloud.google.com/security/secret-manager?project=',
+            type: 'secrets'
+        }
+    },
 ]
 
 //select from a list of options
@@ -27,8 +62,66 @@ const env = await arg("Choose an environment", [
 const url = await arg("Choose a url", urls)
 
 //append env
-const urlWithEnv = `${url}${env}`
+let finalUrl = `${url.url}${env}`
+
+switch (url.type) {
+    case 'run':
+        const cloudRunInstances = await exec(`/opt/homebrew/bin/gcloud run services list --platform=managed --project=${env} --format="json"`)
+        const data = JSON.parse(cloudRunInstances.stdout)
+
+        const instances = data.map(instance => ({
+            name: instance.metadata.name,
+            value: `https://console.cloud.google.com/run/detail/${instance.metadata.labels['cloud.googleapis.com/location']}/${instance.metadata.name}/metrics?project=${env}`,
+        }))
+        const open = {name: 'Open', value: finalUrl}
+
+        finalUrl = await arg("Choose a Cloud Run instance", [open, ...instances])
+        break
+
+    case 'scheduler':
+        const cloudSchedulerInstances = await exec(`/opt/homebrew/bin/gcloud scheduler jobs list --project=${env} --format="json"`)
+        const schedulerData = JSON.parse(cloudSchedulerInstances.stdout)
+
+        const schedulerInstances = schedulerData.map(job => ({
+            name: job.name.split('/').pop(),
+            description: job.description,
+            value: `https://console.cloud.google.com/cloudscheduler/job/${env}/${job.name.split('/').pop()}?project=${env}`,
+        }))
+        const schedulerOpen = {name: 'Open', value: finalUrl}
+
+        finalUrl = await arg("Choose a Cloud Scheduler instance", [schedulerOpen, ...schedulerInstances])
+        break;
+    case 'storage':
+        const storageBuckets = await exec(
+            `/opt/homebrew/bin/gcloud storage buckets list --project=${env} --format="json"`
+        );
+        const storageData = JSON.parse(storageBuckets.stdout);
+
+        const buckets = storageData.map(bucket => ({
+            name: bucket.name,
+            value: `https://console.cloud.google.com/storage/browser/${bucket.name}?project=${env}`,
+        }));
+        const openStorage = { name: 'Open', value: finalUrl };
+
+        finalUrl = await arg('Choose a Storage bucket', [openStorage, ...buckets]);
+        break;
+    case 'secrets':
+        const secretsOutput = await exec(
+            `/opt/homebrew/bin/gcloud secrets list --project=${env} --format="json"`
+        );
+        const secretsData = JSON.parse(secretsOutput.stdout);
+
+        const secrets = secretsData.map(secret => ({
+            name: secret.name.split('/').pop(),
+            value: `https://console.cloud.google.com/security/secret-manager/secret/${secret.name.split('/').pop()}?project=${env}`,
+        }));
+        const openSecrets = { name: 'Open', value: finalUrl };
+
+        finalUrl = await arg('Choose a Secret', [openSecrets, ...secrets]);
+        break;}
+
+await exec(`open -na "Google Chrome" --args --profile-directory="Profile 1" "${finalUrl}"`)
 
 
 //open url in chrome in FF profile
-await exec(`open -na "Google Chrome" --args --profile-directory="Profile 1" "${urlWithEnv}"`)
+// await exec(`open -na "Google Chrome" --args --profile-directory="Profile 1" "${urlWithEnv}"`)
