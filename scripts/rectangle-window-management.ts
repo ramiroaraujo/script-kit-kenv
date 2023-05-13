1e30b804ff4e09b06e9445b18f0ae9006614d8ae
Ramiro Araujo <rama.araujo@gmail.com>
2023-05-13 11:45:53 -0300

    base rectangle scirpt

---
+++ scripts/rectangle-window-management.ts
@@ -0,0 +1,13 @@
+// Name: Rectangle Window Management
+// Shortcut: cmd shift d
+
+import "@johnlindquist/kit"
+import {UI} from "../../../../.kit/core/enum";
+
+let keyInfo = await hotkey({
+
+})
+await hide();
+
+await notify("Rectangle Window Management" )
+
