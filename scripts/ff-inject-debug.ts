d06c5861fd12957eb0235adbc3da48b3393cf937
Ramiro Araujo <rama.araujo@gmail.com>
2023-06-30 10:15:32 -0300

    add remove debug to inject script

--- scripts/ff-inject-debug.ts
+++ scripts/ff-inject-debug.ts
@@ -20,19 +20,38 @@ const validFolders = allFolders.map(async folder => {
 
 const folders = (await Promise.all(validFolders)).filter(Boolean);
 
-const folder = await arg("Select a folder to inject the debug config", folders);
+let flags = {
+    remove: {
+        name: "Remove",
+        shortcut: "cmd+r",
+    },
+}
 
+const folder = await arg({
+    placeholder: "Select a folder to inject the debug config",
+    flags
+}, folders);
+
+const path = home(`FactoryFix/${folder}`)
+const mainPath = `${path}/src/main.ts`;
+const appModulePath = `${path}/src/app.module.ts`;
+const packageJsonPath = `${path}/package.json`;
+const dockerComposePath = `${path}/docker-compose.yml`;
+
+if (flag?.remove) {
+    await exec(`cd ${path} && git update-index --no-assume-unchanged src/main.ts src/app.module.ts package.json docker-compose.yml && git checkout src/main.ts src/app.module.ts package.json docker-compose.yml`);
+    await notify(`Removed Debug config from ${folder}`);
+} else {
 // Inject the debug config
-const mainPath = home(`FactoryFix/${folder}/src/main.ts`);
-let mainContent = await readFile(mainPath, 'utf-8');
+    let mainContent = await readFile(mainPath, 'utf-8');
 
 // 1. Insert "app.enableShutdownHooks();" before the line "await app.listen(port, host)" in src/main.ts
-const listenLineIndex = mainContent.indexOf('await app.listen');
+    const listenLineIndex = mainContent.indexOf('await app.listen');
 //check if app.enableShutdownHooks() is already there
-if (mainContent.search('app.enableShutdownHooks()') === -1) {
-    mainContent = mainContent.slice(0, listenLineIndex) + 'app.enableShutdownHooks();\n\n  ' + mainContent.slice(listenLineIndex);
-}
-await writeFile(mainPath.replace('main.ts', 'main.ts'), mainContent, 'utf-8');
+    if (mainContent.search('app.enableShutdownHooks()') === -1) {
+        mainContent = mainContent.slice(0, listenLineIndex) + 'app.enableShutdownHooks();\n\n  ' + mainContent.slice(listenLineIndex);
+    }
+    await writeFile(mainPath.replace('main.ts', 'main.ts'), mainContent, 'utf-8');
 
 // 2. Replace the line that contains "export class AppModule {}" in src/app.module.ts with the following:
 //    export class AppModule implements OnApplicationShutdown {
@@ -40,61 +59,58 @@ await writeFile(mainPath.replace('main.ts', 'main.ts'), mainContent, 'utf-8');
 //        process.exit(2);
 //      }
 //    }
-const appModulePath = home(`FactoryFix/${folder}/src/app.module.ts`);
-let appModuleContent = await readFile(appModulePath, 'utf-8');
+    let appModuleContent = await readFile(appModulePath, 'utf-8');
 
-const exportClassIndex = appModuleContent.indexOf('export class AppModule');
-const exportClassEndIndex = appModuleContent.indexOf('}', exportClassIndex) + 1;
+    const exportClassIndex = appModuleContent.indexOf('export class AppModule');
+    const exportClassEndIndex = appModuleContent.indexOf('}', exportClassIndex) + 1;
 //check if onApplicationShutdown is already there
-if (appModuleContent.search('implements OnApplicationShutdown') === -1) {
-    appModuleContent = appModuleContent.slice(0, exportClassIndex) +
-        `export class AppModule implements OnApplicationShutdown {
+    if (appModuleContent.search('implements OnApplicationShutdown') === -1) {
+        appModuleContent = appModuleContent.slice(0, exportClassIndex) +
+            `export class AppModule implements OnApplicationShutdown {
   public async onApplicationShutdown(signal?: string): Promise<void> {
     process.exit(2);
   }
 }` +
-        appModuleContent.slice(exportClassEndIndex);
-}
+            appModuleContent.slice(exportClassEndIndex);
+    }
 
 // 3. In src/app.module.ts, add the following import: import { OnApplicationShutdown } from '@nestjs/common';
-const importIndex = appModuleContent.indexOf(" } from '@nestjs/common'");
+    const importIndex = appModuleContent.indexOf(" } from '@nestjs/common'");
 //check if OnApplicationShutdown is already there
-if (appModuleContent.search(', OnApplicationShutdown') === -1) {
-    appModuleContent = appModuleContent.slice(0, importIndex) + ', OnApplicationShutdown' + appModuleContent.slice(importIndex);
-}
+    if (appModuleContent.search(', OnApplicationShutdown') === -1) {
+        appModuleContent = appModuleContent.slice(0, importIndex) + ', OnApplicationShutdown' + appModuleContent.slice(importIndex);
+    }
 
-await writeFile(appModulePath.replace('app.module.ts', 'app.module.ts'), appModuleContent, 'utf-8');
+    await writeFile(appModulePath.replace('app.module.ts', 'app.module.ts'), appModuleContent, 'utf-8');
 
 // Edit package.json
-const packageJsonPath = home(`FactoryFix/${folder}/package.json`);
-const packageJsonContent = await readFile(packageJsonPath, 'utf-8');
-const startDebugRegex = /("start:debug":\s*)"nest start --debug --watch"/;
+    const packageJsonContent = await readFile(packageJsonPath, 'utf-8');
+    const startDebugRegex = /("start:debug":\s*)"nest start --debug --watch"/;
 
-if (startDebugRegex.test(packageJsonContent)) {
-    const updatedPackageJsonContent = packageJsonContent.replace(startDebugRegex, '$1"nest start --debug=0.0.0.0 --watch"');
-    await writeFile(packageJsonPath.replace('package.json', 'package.json'), updatedPackageJsonContent, 'utf-8');
-}
+    if (startDebugRegex.test(packageJsonContent)) {
+        const updatedPackageJsonContent = packageJsonContent.replace(startDebugRegex, '$1"nest start --debug=0.0.0.0 --watch"');
+        await writeFile(packageJsonPath.replace('package.json', 'package.json'), updatedPackageJsonContent, 'utf-8');
+    }
 
 // Edit docker-compose.yml
-const dockerComposePath = home(`FactoryFix/${folder}/docker-compose.yml`);
-const dockerComposeContent = await readFile(dockerComposePath, 'utf-8');
+    const dockerComposeContent = await readFile(dockerComposePath, 'utf-8');
 
-const debugPortRegex = /(-\s+')(\d+):\d+'/;
-const debugPortReplacement = `$1$2:9229'`;
+    const debugPortRegex = /(-\s+')(\d+):\d+'/;
+    const debugPortReplacement = `$1$2:9229'`;
 
-const commandRegex = /command: \['yarn', 'start:dev']/;
-const commandReplacement = `command: ['yarn', 'start:debug']`;
+    const commandRegex = /command: \['yarn', 'start:dev']/;
+    const commandReplacement = `command: ['yarn', 'start:debug']`;
 
-const updatedDockerComposeContent = dockerComposeContent
-    .replace(debugPortRegex, debugPortReplacement)
-    .replace(commandRegex, commandReplacement);
+    const updatedDockerComposeContent = dockerComposeContent
+        .replace(debugPortRegex, debugPortReplacement)
+        .replace(commandRegex, commandReplacement);
 
-await writeFile(dockerComposePath.replace('docker-compose.yml', 'docker-compose.yml'), updatedDockerComposeContent, 'utf-8');
+    await writeFile(dockerComposePath.replace('docker-compose.yml', 'docker-compose.yml'), updatedDockerComposeContent, 'utf-8');
 
 //
 
-const path = home(`FactoryFix/${folder}`)
-await exec(`cd ${path} && git update-index --assume-unchanged src/main.ts && git update-index --assume-unchanged src/app.module.ts && git update-index --assume-unchanged package.json && git update-index --assume-unchanged docker-compose.yml`)
+    await exec(`cd ${path} && git update-index --assume-unchanged src/main.ts && git update-index --assume-unchanged src/app.module.ts && git update-index --assume-unchanged package.json && git update-index --assume-unchanged docker-compose.yml`)
 
 // Notify the user that the folders are being fetched
-await notify(`Injected Debug config to ${folder}`);
+    await notify(`Injected Debug config to ${folder}`);
+}
