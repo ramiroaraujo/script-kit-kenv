f25049f63cf7433d909e5d3a613672e4a4ec55e5
Ramiro Araujo <rama.araujo@gmail.com>
2023-09-06 13:02:19 -0300

    complete extract with jq with proper preview and error handling

--- scripts/extract-with-jq.ts
+++ scripts/extract-with-jq.ts
@@ -1,42 +1,48 @@
 // Name: Extract with jq
-// WIP, should show the JSON content in the preview, parse the input, if it's invalid jq indicate it in hint, but keep
-// showing the JSON. Basically the JSON in the preview should always reflect the result of the jq transform.
-// maybe keep a record of "lastValidjq" and show that in the preview if the current input is invalid.
 
 import "@johnlindquist/kit";
 
 // Fetch clipboard content
-let content = await clipboard.readText();
+const content = await clipboard.readText();
+
+const transform = async (text: string, query: string): Promise<string> => {
+    let response = await exec(`jq ${query}`, {
+        reject: false,
+        input: text,
+        env: {
+            PATH: `${env.PATH}:/opt/homebrew/bin`,
+        },
+    });
+    if (response.failed) {
+        throw new Error('invalid query');
+    }
+    if (response.stdout.trim() === 'null') {
+        throw new Error('null');
+    }
+    return response.stdout;
+}
 
 // Prompt for a jq transform
-let transform = await arg({
+const query = await arg({
     placeholder: "Enter a jq transform:",
-    validate: (input) => false,
-    hint: 'invalid jq'
-
+    ignoreBlur: true,
+    input: '.',
+}, async (input) => {
+    try {
+        const result = await transform(content, input);
+        return md(`<pre style="margin-top: 0">${result}</pre>`);
+    } catch (e) {
+        if (e.message === 'null')
+            return md(`_null results_\n<pre>${content}</pre>`);
+        else
+            return md(`_Invalid query_\n<pre>${content}</pre>`);
+    }
 });
 
 // Apply the jq transformation
-let transformedContent = await exec(`/opt/homebrew/bin/jq '${transform}'`, {
-    input: content,
-    env: {
-        PATH: `${env.PATH}:/opt/homebrew/bin`,
-    },
-});
+const extract = await transform(content, query);
 
-// Show the transformed content in a preview
-let previewContent = transformedContent.all;
-let lines = previewContent.split("\n");
-if (lines.length > 10) {
-    // Show only the first 10 lines, followed by a message about the omitted lines
-    previewContent = lines.slice(0, 10).join("\n") + `\n\n...and ${lines.length - 10} more lines`;
-}
-let previewText = `Transformed content:\n\n${previewContent}`;
-let previewChoices = [{ name: previewText, value: transformedContent.all }];
+// Copy the transformed content to the clipboard
+await clipboard.writeText(extract);
 
-let selectedValue = await arg("Preview:", previewChoices);
-
-// Copy the selected value to clipboard
-await clipboard.writeText(selectedValue);
-
-await notify("Transformed content copied to clipboard.");
+notify('Transformed content copied to clipboard');
