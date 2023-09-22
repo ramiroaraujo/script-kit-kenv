75cace090e1a21423f50516adb761b38cd2023d8
Ramiro Araujo <rama.araujo@gmail.com>
2023-09-22 16:23:36 -0300

    add date presets to cache helper

--- lib/cache-helper.ts
+++ lib/cache-helper.ts
@@ -2,6 +2,31 @@ import {DB} from "../../../../.kit/types/kit";
 
 type DBHelper = ReturnType<DB> extends Promise<infer T> ? T : never;
 
+const datePresets = {
+    "1h": 3600,
+    "2h": 7200,
+    "3h": 10800,
+    "4h": 14400,
+    "5h": 18000,
+    "6h": 21600,
+    "7h": 25200,
+    "8h": 28800,
+    "9h": 32400,
+    "10h": 36000,
+    "11h": 39600,
+    "12h": 43200,
+    "1d": 86400,
+    "2d": 172800,
+    "3d": 259200,
+    "4d": 345600,
+    "5d": 432000,
+    "6d": 518400,
+    "1w": 604800,
+    "2w": 1209600,
+    "1m": 2592000,
+}
+type DatePresets = typeof datePresets;
+
 export class CacheHelper {
 
     private db: DBHelper;
@@ -27,9 +52,9 @@ export class CacheHelper {
         return this;
     }
 
-    setDefaultExpires(defaultExpires: number) {
+    setDefaultExpires(defaultExpires: number | keyof DatePresets) {
         this.failIfInit()
-        this.defaultExpires = defaultExpires;
+        this.defaultExpires = typeof defaultExpires === 'number' ? defaultExpires : datePresets[defaultExpires];
         return this;
     }
 
