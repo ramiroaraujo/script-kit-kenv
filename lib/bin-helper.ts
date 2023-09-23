f844296aff2a82d1a002aeba0cf94befa4c02076
Ramiro Araujo <rama.araujo@gmail.com>
2023-09-23 17:11:06 -0300

    add bin helper to resolve bin paths or warn about it

---
+++ lib/bin-helper.ts
@@ -0,0 +1,26 @@
+import {getEnv} from "./env-helper";
+
+const commonBinPaths = [
+    '/opt/homebrew/bin',
+    '/usr/local/bin',
+    '/usr/bin',
+    '/bin',
+]
+
+export const binPath = async (binName: string) => {
+    //check if bin exists in common paths
+    for(const path of commonBinPaths) {
+        if(await pathExists(`${path}/${binName}`)) {
+            return `${path}/${binName}`
+        }
+    }
+    //check in env
+    for await (const path of getEnv('PATH').split(':')){
+        if(await pathExists(`${path}/${binName}`)) {
+            return `${path}/${binName}`
+        }
+    }
+    const error = `Could not find ${binName} in PATH`;
+    notify({title: error, message: 'Add or update the PATH in Kit .env'})
+    throw new Error(error)
+}
