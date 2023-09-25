fa6a9baf132900e79407c00b1529a9bd63949ca7
Ramiro Araujo <rama.araujo@gmail.com>
2023-09-25 03:06:51 -0300

    Refactored test environment extraction in ff-extract-test-envs.ts
    Removed redundant code for parsing the yaml and reading the file in ff-extract-test-envs.ts. This was done to utilize the already defined getTestEnvs method in FFService class. It simplifies the code and avoids duplication.

--- scripts/ff-extract-test-envs.ts
+++ scripts/ff-extract-test-envs.ts
@@ -4,19 +4,11 @@ import "@johnlindquist/kit"
 import {getFFLocalServices} from "../lib/ff-helper";
 import {FFService} from "../lib/ff-service";
 
-const yaml = await npm('yaml')
-
 const folders = await getFFLocalServices();
 const serviceName = await arg("Select a service to extract test env variables", folders);
 
 const service = await FFService.init(serviceName)
-const testDockerFile = `${service.getPath()}/deployment/test/docker-compose.test.yml`
-const content = await readFile(testDockerFile, 'utf-8')
-
-const config = yaml.parse(content)
-
-debugger
-const envs = config.services.sut.environment;
+const envs = await service.getTestEnvs()
 
 const converted = Object.entries(envs)
     .map(([key, value]) => `${key}=${value}`)
