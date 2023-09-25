acfacc6c3539c03eccaf09992761f0ed781f3491
Ramiro Araujo <rama.araujo@gmail.com>
2023-09-25 13:21:37 -0300

    Update fetch body from request to check clipboard for gcloud log
    The diff improves the script 'fetch body from request' by first checking the clipboard for a gcloud log url or decoded JSON. If it's one of these, the request to fetch can be directly extracted from the stored log information, without needing user to select from services list. If clipboard doesn't contain useful information, the script falls back to the original flow of having the user manually select an environment and service. This update enhances the script's functionality and makes it more user-friendly by minimizing user input wherever possible.

--- scripts/ff-fetch-body-from-request.ts
+++ scripts/ff-fetch-body-from-request.ts
@@ -9,50 +9,138 @@ import relativeTime from "dayjs/plugin/relativeTime.js";
 const dayjs = await npm('dayjs')
 dayjs.extend(relativeTime);
 
+type Request = {
+    httpRequest: {
+        requestUrl: string
+    },
+    resource: {
+        labels: {
+            service_name: string
+        }
+    },
+    timestamp: string,
+    trace: string
+}
+
 const gcloud = await binPath('gcloud')
 
-// select env
-const env = await selectEnv()
+const cache = new CacheHelper().setDefaultExpires('1w')
 
-const cache = new CacheHelper(`ff-fetch-body-from-request-${env}`, '1w');
-await cache.init();
+const clipboardText = await clipboard.readText();
+//check if clipboard text is a gcloud log url or gcloud log JSON
+let fetch = true
+let request:Request
+let env:string
+if (clipboardText.startsWith('https://console.cloud.google.com/logs/query')) {
+    try {
+        const url = new URL(clipboardText)
+        const params = url.pathname
+            .substring('/logs/query;'.length)
+            .split(';')
+            .map((pair) => pair.split('='))
+            .reduce((acc, [key, value]) => ({...acc, [key]: decodeURIComponent(value)}), {} as Record<string, any>);
+        params.query = params.query.split('\n')
+            .map((line) => line.trim().split('='))
+            .reduce((acc, [key, value]) => ({...acc, [key]: value.replace(/^"?([^"]+)"?$/, '$1')}), {})
+        request = {
+            httpRequest: {
+                requestUrl: params.query['httpRequest.requestUrl']
+            },
+            trace: params.query['insertId'],
+            timestamp: params.cursorTimestamp,
+            resource: {
+                labels: {
+                    service_name: params.query['resource.labels.service_name']
+                }
+            }
+        }
+        env = url.searchParams.get('project')
 
-// select cloud run service
-const services = await cache.remember('services', async () => {
-    const {stdout:cloudRunInstances} = await exec(`${gcloud} run services list --platform=managed --project=${env} --format="json"`)
-    return JSON.parse(cloudRunInstances)
-})
+        //init cache after env is defined
+        cache.setKey(`ff-fetch-body-from-request-${env}`)
+        await cache.init();
 
-const serviceNames = services.map(instance => instance.metadata.name)
-
-const serviceName = await arg("Choose a Service", serviceNames)
-
-//fetch requests from logs, filter with jq to get unique URLs
-const logs = await cache.remember(`logs-${serviceName}`, async () => {
-    const {stdout:logs} = await exec(`${gcloud} logging read 'resource.labels.service_name="${serviceName}" AND httpRequest.requestMethod="POST" OR httpRequest.requestMethod="PUT" AND logName:"requests"' --limit=1000 --project=${env} --format="json"`)
-    return JSON.parse(logs)
-})
-
-const requests = logs.map(log => {
-    const { httpRequest: { status, requestUrl, requestMethod: method } } = log;
-    const url = new URL(requestUrl)
-    const dateAgo = dayjs(log.timestamp).fromNow()
-    return {
-        name: `${method} ${url.pathname} (${status}) ${dateAgo}`,
-        value: log
+        fetch = await arg('Do you want the Request that belongs the clipboard log?', [
+            { name: 'Yes', value: false },
+            { name: 'No, select from the list of Services', value: true },
+        ])
+    } catch (e) {
+        log(e)
     }
-})
-const request = await arg<any>({
-    placeholder: "Choose a Request",
+}
+try {
+    const json = JSON.parse(clipboardText)
+    request = {
+        httpRequest: {
+            requestUrl: json.httpRequest.requestUrl
+        },
+        trace: json.trace,
+        timestamp: json.timestamp,
+        resource: {
+            labels: {
+                service_name: json.resource.labels.service_name
+            }
+        }
+    }
+    env = json.resource.labels.project_id
 
-}, requests)
+    //init cache after env is defined
+    cache.setKey(`ff-fetch-body-from-request-${env}`)
+    await cache.init();
+
+    fetch = await arg('Do you want the Request that belongs the clipboard log?', [
+        { name: 'Yes', value: false },
+        { name: 'No, select from the list of Services', value: true },
+    ])
+} catch (e) {
+    log(e)
+}
+
+if (fetch) {
+    // select env
+    env = await selectEnv()
+
+    //init cache after env is defined
+    cache.setKey(`ff-fetch-body-from-request-${env}`)
+    await cache.init();
+
+    // select cloud run service
+    const services = await cache.remember('services', async () => {
+        const {stdout:cloudRunInstances} = await exec(`${gcloud} run services list --platform=managed --project=${env} --format="json"`)
+        return JSON.parse(cloudRunInstances)
+    })
+
+    const serviceNames = services.map(instance => instance.metadata.name)
+
+    const serviceName = await arg("Choose a Service", serviceNames)
+
+    //fetch requests from logs, filter with jq to get unique URLs
+    const logs = await cache.remember(`logs-${serviceName}`, async () => {
+        const {stdout:logs} = await exec(`${gcloud} logging read 'resource.labels.service_name="${serviceName}" AND httpRequest.requestMethod="POST" OR httpRequest.requestMethod="PUT" AND logName:"requests"' --limit=1000 --project=${env} --format="json"`)
+        return JSON.parse(logs)
+    })
+
+    const requests = logs.map(log => {
+        const { httpRequest: { status, requestUrl, requestMethod: method } } = log;
+        const url = new URL(requestUrl)
+        const dateAgo = dayjs(log.timestamp).fromNow()
+        return {
+            name: `${method} ${url.pathname} (${status}) ${dateAgo}`,
+            value: log
+        }
+    })
+    request = await arg<Request>({
+        placeholder: "Choose a Request",
+    }, requests)
+}
 
 //fetch debug log for request
+debugger
 const debugLog = await cache.remember(`debug-log-${request.trace}`, async () => {
-    const { httpRequest: { requestUrl } } = request;
+    debugger;
+    const { httpRequest: { requestUrl }, resource: { labels: {service_name}}, timestamp } = request;
     const path = new URL(requestUrl).pathname
-    const timestamp = request.timestamp
-    const {stdout:log} = await exec(`${gcloud} logging read 'resource.labels.service_name="${serviceName}" AND severity="DEBUG" AND labels.path="${path}" AND timestamp>="${timestamp}"' --limit=1 --project=${env} --format="json"`)
+    const {stdout:log} = await exec(`${gcloud} logging read 'resource.labels.service_name="${service_name}" AND severity="DEBUG" AND labels.path="${path}" AND timestamp>="${timestamp}"' --limit=1 --project=${env} --format="json"`)
     const response = JSON.parse(log);
     return response.length ? response[0] : null
 })
