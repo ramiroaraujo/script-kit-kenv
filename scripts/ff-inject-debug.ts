d13eb10af22a19eccf4be0e0f302e3fb8d12e3e6
Ramiro Araujo <rama.araujo@gmail.com>
2023-09-24 22:48:46 -0300

    Refactor ff-inject-debug script to use ff-service and ff-helper
    The commit updates the ff-inject-debug.ts script to use the abstracted ff-service and ff-helper. This simplifies the script leading to improved readability and maintainability. Specifically, the logic for getting valid folders has been moved to the ff-helper's getFFLocalServices function, and the logic for creating and interacting with FFService instances has been moved to the FFService class. Additionally, unnecessary async-await calls were removed from the notify function calls.

--- scripts/ff-inject-debug.ts
+++ scripts/ff-inject-debug.ts
@@ -1,38 +1,16 @@
 // Name: ff inject debug
 
 import "@johnlindquist/kit";
-
-// For each folder, filter out the ones that are not a Nest application
-
-const allFolders = (await exec(`cd ~/FactoryFix && ls -d */`)).all.split('\n').map(folder => folder.replace('/', ''));
-
-const validFolders = allFolders.map(async folder => {
-    try {
-        let path = home(`FactoryFix/${folder}/package.json`);
-        const file = await readFile(path, 'utf-8');
-        const packageJson = JSON.parse(file);
-
-        return packageJson.dependencies['@nestjs/core'] ? folder : null;
-    } catch (e) {
-        return null;
-    }
-});
-
-const folders = (await Promise.all(validFolders)).filter(Boolean);
-
-let flags = {
-    remove: {
-        name: "Remove",
-        shortcut: "cmd+r",
-    },
-}
+import {getFFLocalServices} from "../lib/ff-helper";
+import {FFService} from "../lib/ff-service";
 
 const folder = await arg({
     placeholder: "Select a folder to inject the debug config",
-    flags
-}, folders);
+    flags: { remove: { name: "Remove", shortcut: "cmd+r" } }
+}, await getFFLocalServices());
 
-const path = home(`FactoryFix/${folder}`)
+const service = await FFService.init(folder);
+const path = service.getPath()
 const mainPath = `${path}/src/main.ts`;
 const appModulePath = `${path}/src/app.module.ts`;
 const packageJsonPath = `${path}/package.json`;
@@ -40,30 +18,30 @@ const dockerComposePath = `${path}/docker-compose.yml`;
 
 if (flag?.remove) {
     await exec(`cd ${path} && git update-index --no-assume-unchanged src/main.ts src/app.module.ts package.json docker-compose.yml && git checkout src/main.ts src/app.module.ts package.json docker-compose.yml`);
-    await notify(`Removed Debug config from ${folder}`);
+    notify(`Removed Debug config from ${folder}`);
 } else {
-// Inject the debug config
+    // Inject the debug config
     let mainContent = await readFile(mainPath, 'utf-8');
 
-// 1. Insert "app.enableShutdownHooks();" before the line "await app.listen(port, host)" in src/main.ts
+    // 1. Insert "app.enableShutdownHooks();" before the line "await app.listen(port, host)" in src/main.ts
     const listenLineIndex = mainContent.indexOf('await app.listen');
-//check if app.enableShutdownHooks() is already there
+    //check if app.enableShutdownHooks() is already there
     if (mainContent.search('app.enableShutdownHooks()') === -1) {
         mainContent = mainContent.slice(0, listenLineIndex) + 'app.enableShutdownHooks();\n\n  ' + mainContent.slice(listenLineIndex);
     }
     await writeFile(mainPath.replace('main.ts', 'main.ts'), mainContent, 'utf-8');
 
-// 2. Replace the line that contains "export class AppModule {}" in src/app.module.ts with the following:
-//    export class AppModule implements OnApplicationShutdown {
-//      public async onApplicationShutdown(signal?: string): Promise<void> {
-//        process.exit(2);
-//      }
-//    }
+    // 2. Replace the line that contains "export class AppModule {}" in src/app.module.ts with the following:
+    //    export class AppModule implements OnApplicationShutdown {
+    //      public async onApplicationShutdown(signal?: string): Promise<void> {
+    //        process.exit(2);
+    //      }
+    //    }
     let appModuleContent = await readFile(appModulePath, 'utf-8');
 
     const exportClassIndex = appModuleContent.indexOf('export class AppModule');
     const exportClassEndIndex = appModuleContent.indexOf('}', exportClassIndex) + 1;
-//check if onApplicationShutdown is already there
+    //check if onApplicationShutdown is already there
     if (appModuleContent.search('implements OnApplicationShutdown') === -1) {
         appModuleContent = appModuleContent.slice(0, exportClassIndex) +
             `export class AppModule implements OnApplicationShutdown {
@@ -74,16 +52,16 @@ if (flag?.remove) {
             appModuleContent.slice(exportClassEndIndex);
     }
 
-// 3. In src/app.module.ts, add the following import: import { OnApplicationShutdown } from '@nestjs/common';
+    // 3. In src/app.module.ts, add the following import: import { OnApplicationShutdown } from '@nestjs/common';
     const importIndex = appModuleContent.indexOf(" } from '@nestjs/common'");
-//check if OnApplicationShutdown is already there
+    //check if OnApplicationShutdown is already there
     if (appModuleContent.search(', OnApplicationShutdown') === -1) {
         appModuleContent = appModuleContent.slice(0, importIndex) + ', OnApplicationShutdown' + appModuleContent.slice(importIndex);
     }
 
     await writeFile(appModulePath.replace('app.module.ts', 'app.module.ts'), appModuleContent, 'utf-8');
 
-// Edit package.json
+    // Edit package.json
     const packageJsonContent = await readFile(packageJsonPath, 'utf-8');
     const startDebugRegex = /("start:debug":\s*)"nest start --debug --watch"/;
 
@@ -92,7 +70,7 @@ if (flag?.remove) {
         await writeFile(packageJsonPath.replace('package.json', 'package.json'), updatedPackageJsonContent, 'utf-8');
     }
 
-// Edit docker-compose.yml
+    // Edit docker-compose.yml
     const dockerComposeContent = await readFile(dockerComposePath, 'utf-8');
 
     const debugPortRegex = /(-\s+')(\d+):\d+'/;
@@ -111,6 +89,6 @@ if (flag?.remove) {
 
     await exec(`cd ${path} && git update-index --assume-unchanged src/main.ts && git update-index --assume-unchanged src/app.module.ts && git update-index --assume-unchanged package.json && git update-index --assume-unchanged docker-compose.yml`)
 
-// Notify the user that the folders are being fetched
-    await notify(`Injected Debug config to ${folder}`);
+    // Notify the user that the folders are being fetched
+    notify(`Injected Debug config to ${folder}`);
 }
