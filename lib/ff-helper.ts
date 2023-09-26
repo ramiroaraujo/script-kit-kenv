1886c16b3c7c119a8ccaeb2e2e4a41da3cd73d1d
Ramiro Araujo <rama.araujo@gmail.com>
2023-09-26 11:12:21 -0300

    Fix BUG after refactor in getFFPath

--- lib/ff-helper.ts
+++ lib/ff-helper.ts
@@ -40,11 +40,9 @@ export const getFFLocalServices = async (nestOnly = true) => {
 
 export const getFFPath = async (): Promise<string> => {
   const defaultPath = home('FactoryFix');
-  if (hasEnv('FF_PATH')) {
-    const env = getEnv('FF_PATH', defaultPath);
-    if (await isDir(env)) {
-      return env;
-    }
+  const env = getEnv('FF_PATH', defaultPath);
+  if (await isDir(env)) {
+    return env;
   }
 
   const error = `Could not find FF_PATH in .env or ${defaultPath} does not exist`;
