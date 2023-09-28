2a654decc69ff755440a1d464270c19c2db8f6c5
Ramiro Araujo <rama.araujo@gmail.com>
2023-09-28 00:47:51 -0300

    refactor ff-helper for better typing and export envs

--- lib/ff-helper.ts
+++ lib/ff-helper.ts
@@ -1,20 +1,26 @@
 import { getEnv } from './env-helper';
 import { FFService } from './ff-service';
+import * as fs from 'fs';
+
+export const environments = {
+  dev: 'ff-app-dev',
+  iso1: 'ff-app-iso-1',
+  iso2: 'ff-app-iso-2',
+  iso3: 'ff-app-iso-3',
+  iso4: 'ff-app-iso-4',
+  e2e: 'ff-app-e2e',
+  prod: 'ff-app-prod',
+};
+
+type FFEnv = keyof typeof environments;
 
 export const selectEnv = async (withoutPro = false) => {
-  const envs = [
-    'ff-app-dev',
-    'ff-app-iso-1',
-    'ff-app-iso-2',
-    'ff-app-iso-3',
-    'ff-app-iso-4',
-    'ff-app-e2e',
-  ];
-  if (!withoutPro) {
-    envs.unshift('ff-app-prod');
+  let envs = Object.values(environments);
+  if (withoutPro) {
+    envs = envs.filter((env) => env !== environments.prod);
   }
 
-  return await arg('Choose an environment', envs);
+  return await arg<FFEnv>('Choose an environment', envs);
 };
 
 export const getFFLocalServices = async (nestOnly = true) => {
@@ -35,12 +41,12 @@ export const getFFLocalServices = async (nestOnly = true) => {
     });
     folders = (await Promise.all(validFolders)).filter(Boolean);
   }
-  return folders;
+  return folders as string[];
 };
 
-export const getFFPath = async (): Promise<string> => {
+export const getFFPath = () => {
   const env = getEnv('FF_PATH');
-  if (await isDir(env)) {
+  if (fs.existsSync(env)) {
     return env;
   }
 
