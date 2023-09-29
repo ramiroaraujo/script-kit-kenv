e84457e769f612acded895350a95f8d6b710daa1
Ramiro Araujo <rama.araujo@gmail.com>
2023-09-29 00:05:15 -0300

    add clipboard-service for managing alfred clipboard history

---
+++ lib/clipboard-service.ts
@@ -0,0 +1,52 @@
+await npm('better-sqlite3');
+
+import '@johnlindquist/kit';
+import Database, { Database as DatabaseType } from 'better-sqlite3';
+import { getEnv } from './env-helper';
+
+type ClipboardItem = {
+  ROWID: number;
+  item: string;
+  ts: number;
+  app: string;
+  apppath: string;
+  dataType: number;
+};
+
+type ClipboardItemTypes = ClipboardItem[keyof ClipboardItem];
+
+export class ClipboardService {
+  private db: DatabaseType;
+
+  constructor() {
+    const databasePath = getEnv('ALFRED_DATABASE_PATH');
+    this.db = new Database(databasePath);
+  }
+
+  getLatest(count: number) {
+    const sql = `SELECT ROWID, *
+                 FROM clipboard
+                 WHERE dataType = 0
+                 order by ROWID desc
+                 LIMIT ?`;
+    return this.db.prepare(sql).all(count) as ClipboardItem[];
+  }
+
+  deleteLatest(count: number) {
+    const sql = `DELETE
+                 FROM clipboard
+                 WHERE ROWID IN (SELECT ROWID FROM clipboard WHERE dataType = 0 ORDER BY ROWID DESC LIMIT ?)`;
+    return this.db.prepare(sql).run(count);
+  }
+
+  raw(sql: string, params: ClipboardItemTypes[] = []) {
+    const stmt = this.db.prepare(sql);
+    return sql.trim().toUpperCase().startsWith('SELECT')
+      ? (stmt.all(params) as ClipboardItemTypes[])
+      : stmt.run(params);
+  }
+
+  close() {
+    this.db.close();
+  }
+}
