c31bf4be4cc010390255b54736c0701a9f1e75c5
Ramiro Araujo <rama.araujo@gmail.com>
2025-06-24 10:35:05 -0300

    remove breath script

--- scripts/breath.ts
+++
@@ -1,7 +0,0 @@
-// Name: Breath
-// Schedule: */30 9-20 * * *
-
-import '@johnlindquist/kit';
-
-await playAudioFile(projectPath('assets/breath-notification.m4a'));
-notify({ title: `Breath in, breath out. 🧘‍♂️`, subtitle: 'Take a deep breath', silent: true });
